dac3d73babcf50faa12ea4253ddfd23eef81889b
gonzalo grisafi <gonzagrisa@gmail.com>
2020-01-07 18:33:07 -0300

    Added route protection

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import store from '../store/index';
 
 Vue.use(VueRouter)
 
@@ -19,24 +20,24 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path:'/login',
-    name:'login',
+    path: '/login',
+    name: 'login',
     component: () => import('../views/Login.vue')
   },
   {
-    path:'/register',
-    name:'register',
-    component: () => import ('../views/Register.vue')
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/Register.vue')
   },
   {
-    path:'/terms',
-    name:'terms',
-    component: () => import ('../views/Terms.vue')
+    path: '/terms',
+    name: 'terms',
+    component: () => import('../views/Terms.vue')
   },
   {
-    path:'/books',
-    name:'books',
-    component: () => import ('../views/Books.vue')
+    path: '/books',
+    name: 'books',
+    component: () => import('../views/Books.vue')
   }
 ]
 
@@ -45,5 +46,19 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.fullPath == '/login' || to.fullPath == '/register') {
+    if (store.state.loggedIn) {
+      console.log("Logged IN");
+      next('/books');
+    }
+    else {
+      next();
+    }
+  }
+  next();
+});
+
 
 export default router
