f431246cee8a6db535e6c013cca5c30d6b331f54
gonzalo grisafi <gonzagrisa@gmail.com>
2020-01-10 20:37:04 -0300

    Added Search.vue route

--- src/router/index.js
+++ src/router/index.js
@@ -45,6 +45,12 @@ const routes = [
     name: 'books',
     component: () => import('../views/Books.vue'),
     meta: { requiresNavBar: true }
+  },
+  {
+    path:'/search',
+    name:'search',
+    component: () => import('../views/Search.vue'),
+    meta: {requiresNavBar: true }
   }
 ]
 
