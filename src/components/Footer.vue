b5b189e63ca97ad5d9f8f077e98630f59e015607
gonzalo grisafi <gonzagrisa@gmail.com>
2020-01-11 19:55:42 -0300

    Now the text below is not selectable

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,25 +10,23 @@
 				rounded
 				class="my-2"
 			>{{ link.name }}</v-btn>
-			<v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
-				{{ new Date().getFullYear() }} —
+
+			<v-col class="primary lighten-2 py-4 text-center white--text foot" cols="12">
+				{{ new Date().getFullYear() }} -
 				<strong>Gonzi System</strong>
 			</v-col>
 			<v-col class="primary lighten-2 text-center white--text" cols="12">
-				<strong>Made with</strong>
-				<v-icon color="red" size="20px">mdi-heart</v-icon>
-				<strong>and</strong>
-				<v-icon color="green">mdi-nodejs</v-icon>
-				<p></p>
-				<v-btn
-					target="_blank"
-					href="https://github.com/gonzagrisa/library_frontend"
-					text
-					rounded
-				>
-					<v-icon>mdi-github-circle</v-icon>
+				<div class="foot">
+					<strong>Made with </strong>
+					<v-icon color="red" size="25px">mdi-heart</v-icon>
+					<strong> and </strong>
+					<v-icon color="green" size="30px">mdi-nodejs</v-icon>
+					<p></p>
+				</div>
+				<v-btn fab target="_blank" href="https://github.com/gonzagrisa/library_frontend" text>
+					<v-icon size="40px">mdi-github-circle</v-icon>
 				</v-btn>
-        <p></p>
+				<p></p>
 			</v-col>
 		</v-row>
 	</v-footer>
@@ -47,3 +45,12 @@ export default {
 	})
 };
 </script>
+
+<style scoped>
+.foot {
+	user-select: none;
+	-webkit-user-select: none;
+	-moz-user-select: none;
+	font-size: 17px;
+}
+</style>
