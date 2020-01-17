cc4d97bca97156ceecba22e0de7838bce30cac20
gonzalo grisafi <gonzagrisa@gmail.com>
2020-01-17 20:54:44 -0300

    View to be shown to users when they want to access an admin route

---
+++ src/components/Unauthorized.vue
@@ -0,0 +1,13 @@
+<template>
+	<v-app id="NotFound">
+		<div id="E404">
+			<p class="Eimg"><v-img src="https://i.imgur.com/A040Lxr.png" max-height="400" max-width="400"></v-img></p>
+			<p class="Etext display-3 font-weight-black">Oops!</p>
+			<p class="Etext display-4 font-weight-black">Error 404</p>
+			<p class="Etext display-2">
+				Esta página se
+				<br />perdió en el Espacio
+			</p>
+		</div>
+	</v-app>
+</template>
