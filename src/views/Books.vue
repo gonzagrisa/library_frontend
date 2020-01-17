dc88c736b61b1177894bc10db07532cd5f302545
gonzalo grisafi <gonzagrisa@gmail.com>
2020-01-16 21:04:48 -0300

    Explore view looking better, card books react on hover

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,46 +1,38 @@
 <template>
 	<div class="team">
 		<v-container fluid>
-			<v-row>
-				<v-col cols="12">
-					<v-row :align="alignment" :justify="justify" class="grey lighten-5" style="height: 300px;">
-						<v-card v-for="(book,index) in books" :key="index" class="ma-3 pa-6" outlined tile>{{book.title}}</v-card>
-					</v-row>
+			<v-row align="center" justify="center">
+				<v-col v-for="(book,index) in books" :key="index" cols="3" md4>
+					<v-hover v-slot:default="{ hover }">
+						<v-card
+						id="card"
+						
+						:elevation="hover ? 18 : 1"
+						@click="console.log('asdasdasd')"
+						>
+							<v-img
+								v-if="book.cover"
+								:src="book.cover"
+								class="white--text align-end"
+								cover
+							>
+								<v-card-title v-text="book.title"></v-card-title>
+							</v-img>
+							<v-img
+								v-else
+								src="../../public/no_cover.jpg"
+								class="white--text align-end"
+								gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.2)"
+								cover
+							>
+								<v-card-title v-text="book.title"></v-card-title>
+							</v-img>
+						</v-card>
+					</v-hover>
 				</v-col>
 			</v-row>
 		</v-container>
-
-		<!-- <v-container class="my-5">
-			<v-layout row wrap>
-				<v-flex xs12 sm6 md4 lg3 v-for="book in books.data" :key="book.id">
-					<v-card max-width="344" class="mx-auto">
-						<v-list-item>
-							<v-list-item-avatar color="grey"></v-list-item-avatar>
-							<v-list-item-content>
-								<v-list-item-title class="headline">Our Changing Planet</v-list-item-title>
-								<v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>
-							</v-list-item-content>
-						</v-list-item>
-
-						<v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="194"></v-img>
-
-						<v-card-text>Visit ten places on our planet that are undergoing the biggest changes today.</v-card-text>
-
-						<v-card-actions>
-							<v-btn text color="deep-purple accent-4">Read</v-btn>
-							<v-btn text color="deep-purple accent-4">Bookmark</v-btn>
-							<v-spacer></v-spacer>
-							<v-btn icon>
-								<v-icon>mdi-heart</v-icon>
-							</v-btn>
-							<v-btn icon>
-								<v-icon>mdi-share-variant</v-icon>
-							</v-btn>
-						</v-card-actions>
-					</v-card>
-				</v-flex>
-			</v-layout>
-		</v-container>-->
+		<br>
 	</div>
 </template>
 
@@ -53,18 +45,34 @@ export default {
 			books: null,
 			alignment: "center",
 			dense: false,
-			justify: "center"
+			justify: "center",
+			noCover: "../../public/no_cover.jpg"
 		};
 	},
-	methods: {
-		
-	},
+	methods: {},
 	mounted() {
-		axios
-			.get("http://localhost:8080/books")
-			.then(response => {
-				this.books = response.data.data
-			});
+		axios.get("http://localhost:8080/books").then(response => {
+			this.books = response.data.data;
+		});
 	}
 };
 </script>
+
+<style scoped>
+#card{
+	width: 200px;
+	height: 300px;
+	position: relative;
+	float: left;
+	transition: 0.2s;
+
+}
+#card:hover{
+  width: 210px;
+  margin-left: -0.3em;
+  margin-top: -0.5em;
+  transition: 0.5s;
+  z-index: 2;
+}
+
+</style>
