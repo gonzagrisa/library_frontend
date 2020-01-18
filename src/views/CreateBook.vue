8a8fcc1265322ae7c69be415f8d84b6b561d37cc
gonzalo grisafi <gonzagrisa@gmail.com>
2020-01-18 18:19:11 -0300

    The view looks better, added rules to verify inputs and changed style

--- src/views/CreateBook.vue
+++ src/views/CreateBook.vue
@@ -1,10 +1,10 @@
 <template>
 	<v-app>
-		<v-card id="rounded-card" class="mx-auto mt-12" width="700">
-			<v-card-title class="justify-center">
-				<h1 class="display-1">Crea tu Cuenta</h1>
+		<v-card id="rounded-card" class="mx-auto mt-12 mb-12" width="700">
+			<v-card-title class="pt-10 justify-center">
+				<h1 class="mb-5">Registrar Libro</h1>
 			</v-card-title>
-			<v-card-text>
+			<v-card-text class="pr-12 pl-12">
 				<v-form v-model="valid">
 					<v-text-field
 						label="Título"
@@ -12,6 +12,7 @@
 						required
 						v-model="title"
 						:rules="this.titleRules"
+						class="mb-2"
 					/>
 					<v-text-field
 						label="Autor"
@@ -19,15 +20,26 @@
 						v-model="author"
 						required
 						:rules="this.authorRules"
+						class="mb-5"
 					/>
-					<v-text-field
-						label="Cantidad"
-						prepend-icon="mdi-bookshelf"
-						v-model="amount"
-						required
-					/>
-					<v-textarea v-model="url" color="teal" prepend-icon="mdi-link-variant"
-                            :rules="this.urlRules">
+					<div id="amount">
+						<v-text-field
+							label="Cantidad"
+							prepend-icon="mdi-bookshelf"
+							type="number"
+							outlined
+							v-model="amount"
+							:rules="[amountRules]"
+							required
+							id="amount"
+						/>
+					</div>
+					<v-textarea
+						v-model="url"
+						color="teal"
+						prepend-icon="mdi-link-variant"
+						:rules="[this.urlRules]"
+					>
 						<template v-slot:label>
 							<div>
 								Url Cover
@@ -38,7 +50,7 @@
 				</v-form>
 				<v-divider></v-divider>
 				<v-card-actions class="justify-center mt-2 mb-2">
-					<v-btn :disabled="!valid" color="success" class="mr-4" rounded>Registrarse</v-btn>
+					<v-btn :disabled="!valid" color="success" class="mr-4" @click="createBook" rounded>Agregar Libro</v-btn>
 				</v-card-actions>
 			</v-card-text>
 		</v-card>
@@ -47,31 +59,62 @@
 
 
 <script>
+import Axios from "axios";
+Axios.defaults.withCredentials = true;
+
 export default {
 	data() {
 		return {
-            valid: false,
+			valid: false,
 			title: "",
 			author: "",
-			amount: 0,
-            url: "",
-            titleRules:[
-                v => !!v || "Ingrese un Título"
-            ],
-            authorRules:[
-                v => !!v || "Ingrese el Autor del Libro"
-            ],
-            urlRules:[
-                v => /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+(?:png|jpg|jpeg|svg)+$/.test(v)
-                    || "Ingrese la Url de una imagen"
-            ]
-		}
+			amount: null,
+			url: null,
+			titleRules: [v => !!v || "Ingrese un Título"],
+			authorRules: [v => !!v || "Ingrese el Autor del Libro"],
+			amountRules: v => {
+				if (!v) return "Ingrese la Cantidad de Ejemplares";
+				if (v < 0)
+					return "La Cantidad de Ejemplares no puede ser negativa";
+				return true;
+			},
+			urlRules: v => {
+				if (!v) return true;
+				if (
+					!/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+(?:png|jpg|jpeg|svg)+$/.test(
+						v
+					)
+				)
+					return "Ingrese la Url de una imagen";
+				return false;
+			}
+		};
+	},
+	methods: {
+		createBook() {
+			Axios.post("http://localhost:8080/books", {
+				title: this.title,
+				author: this.author,
+				amount: this.amount,
+				cover: this.url
+			})
+			.then(response=>{
+				console.log("LIBRO PUBLICADO")
+			})
+			.catch(error => {
+				console.log(error.response);
+			});
+		},
 	}
 };
 </script>
 
 <style scoped>
-#rounded-card{
-    border-radius:15px;
+#rounded-card {
+	border-radius: 15px;
+}
+
+#amount >>> .v-input__slot {
+	width: 160px;
 }
 </style>
