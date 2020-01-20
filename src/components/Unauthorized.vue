b47738c01ccce8f7a5edbdcf6ec28d71f98d731e
gonzalo grisafi <gonzagrisa@gmail.com>
2020-01-20 16:54:39 -0300

    Changed component's look

--- src/components/Unauthorized.vue
+++ src/components/Unauthorized.vue
@@ -1,9 +1,10 @@
 <template>
 	<v-app id="notAuth">
-		<div>
-			<v-img contain src="http://4.bp.blogspot.com/-eA6rkhyTL7U/U1f6Gh08z7I/AAAAAAAAHR4/yU_-Jgdo9Zk/s1600/Police1.jpg">
+		<div class="mx-auto">
+			<v-img src="http://4.bp.blogspot.com/-eA6rkhyTL7U/U1f6Gh08z7I/AAAAAAAAHR4/yU_-Jgdo9Zk/s1600/Police1.jpg">
 
 			</v-img>
 		</div>
 	</v-app>
 </template>
+
